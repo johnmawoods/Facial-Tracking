7f7d928e392be02cf0d64f40d2c063c71402ee28
Davar Daeinejad <davar.daei@gmail.com>
2021-11-16 23:32:59 -0500

    added esc button for exiting

--- examples/easy3d_learning.cpp
+++ examples/easy3d_learning.cpp
@@ -1,5 +1,21 @@
 #include "utility.h"
 
+#include "3rd_party/glfw/include/GLFW/glfw3.h"	// for the KEYs
+
+namespace asu {
+	class CustomViewer : public easy3d::Viewer {
+	public:
+		CustomViewer(const std::string& title) : easy3d::Viewer(title) {}
+
+	protected:
+		bool key_press_event(int key, int modifiers) override {
+			if (key == GLFW_KEY_ESCAPE && modifiers == 0)
+				this->exit();
+			else
+				return easy3d::Viewer::key_press_event(key, modifiers);
+		}
+	};
+}
 
 int main() {
 
@@ -10,10 +26,30 @@ int main() {
 
 	//-------------------------------- viewer
 	//===================================================================================
-	easy3d::Viewer viewer("surface mesh");
+	asu::CustomViewer viewer("custom viewer");   // added the esc button for exiting
 	easy3d::Camera* cam = viewer.camera();       // visualized axes at the bottom left of the screen are red(x), green(y), blue(z) 
 	cam->setUpVector(easy3d::vec3(0, 1, 0));
 	cam->setViewDirection(easy3d::vec3(0, 0, -1));
+
+	//-------------------------------- point cloud
+	//===================================================================================
+	easy3d::PointCloud* cloud = new easy3d::PointCloud;
+	for (float i = -2; i <= 2; ++i) {
+		for (float j = -2; j <= 2; ++j)
+			cloud->add_vertex(easy3d::vec3(i, j, 0));  // z = 0: all points are on XY plane.
+	}
+	auto colors = cloud->add_vertex_property<easy3d::vec3>("v:color");    // per vertex properties are: color, normal and point
+	for (auto v : cloud->vertices())
+		colors[v] = easy3d::random_color();		        // assign a random color to point 'v'
+
+	viewer.add_model(cloud);             // renderer and manipulator are initialized after adding the model, so use them after this line
+	auto vDrawable = cloud->renderer()->get_points_drawable("vertices");   // the string must be "vertices"
+	//vDrawable->set_uniform_coloring(easy3d::vec4(0.8, 0, 0.0, 1.0));
+	vDrawable->set_impostor_type(easy3d::PointsDrawable::SPHERE);
+	vDrawable->set_point_size(20);
+	float pi = 3.1415926;
+	cloud->manipulator()->frame()->rotate(easy3d::quat(easy3d::vec3(0, 0, 1), pi / 4.0));    // 45 degrees around z
+	cloud->manipulator()->frame()->translate(easy3d::vec3(0, 0, 1));
 
 	//-------------------------------- surface mesh
 	//===================================================================================
@@ -30,26 +66,6 @@ int main() {
 	auto sDrawable = mesh->renderer()->get_triangles_drawable("faces");    // the string must be "faces"
 	sDrawable->set_smooth_shading(true);
 	sDrawable->set_uniform_coloring(easy3d::vec4(0.8, 0.8, 0.8, 1.0));
-
-	//-------------------------------- point cloud
-	//===================================================================================
-	easy3d::PointCloud* cloud = new easy3d::PointCloud;
-	for (float i = -2; i <= 2; ++i) {
-		for (float j = -2; j <= 2; ++j)
-			cloud->add_vertex(easy3d::vec3(i, j, 0));  // z = 0: all points are on XY plane.
-	}
-	auto colors = cloud->add_vertex_property<easy3d::vec3>("v:color");    // per vertex properties are: color, normal and point
-	for (auto v : cloud->vertices()) 
-		colors[v] = easy3d::random_color();		        // assign a random color to point 'v'
-	
-	viewer.add_model(cloud);             // renderer and manipulator are initialized after adding the model, so use them after this line
-	auto vDrawable = cloud->renderer()->get_points_drawable("vertices");   // the string must be "vertices"
-	//vDrawable->set_uniform_coloring(easy3d::vec4(0.8, 0, 0.0, 1.0));
-	vDrawable->set_impostor_type(easy3d::PointsDrawable::SPHERE);
-	vDrawable->set_point_size(20);
-	float pi = 3.1415926;
-	cloud->manipulator()->frame()->rotate(easy3d::quat(easy3d::vec3(0, 0, 1), pi / 4.0));    // 45 degrees around z
-	cloud->manipulator()->frame()->translate(easy3d::vec3(0, 0, 1));
 
 	//-------------------------------- rendering
 	//===================================================================================
